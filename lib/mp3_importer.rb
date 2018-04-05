acb069ef21dc78b754b02a2f38a423b414d50990
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-05 14:59:37 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class MP3Importer
+
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        Dir["#{@path}/*.mp3"].collect do |file|
+            file.match(/[A-Z]\w*[^.]*(....)/)[0]
+        end
+    end
+
+    def import
+        self.files.map do |file_name|
+            song = Song.new_by_filename(file_name)
+            song.artist.save
+
+        end
+    end
+
+
+
+
+
+end
